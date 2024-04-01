6819e3f362c5a061df17c718f6816818b5b0d97b
sanjai2002 <sanjaihari2002@gmail.com>
2024-04-01 11:45:15 +0530

    test files added

---
+++ UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Login.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
